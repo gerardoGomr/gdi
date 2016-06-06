1e87af32f12d5beb1f7673f0bf2ddccac6bf30b8
Gerardo Adrián Gómez Ruiz <gerardo.gomr@gmail.com>
2016-06-06 12:16:56 -0500

    Commit inicial, configurando el front end

---
+++ resources/views/login.blade.php
@@ -0,0 +1,48 @@
+@extends('app_no_sidebar')
+
+@section('contenido')
+	<div class="row row-app">
+		<div class="col-separator col-unscrollable box">
+			<div class="col-table">
+				<h4 class="innerAll bg-gray text-center"><i class="fa fa-lock"></i> Grupo de Desarrollo Integral del Transporte</h4>
+				<div class="col-table-row">
+					<div class="col-app col-unscrollable">
+						<div class="col-app">
+							<div class="login">
+								<div class="placeholder text-center"><img src="{{ asset('public/img/logo.png') }}" style="width:300px; border: none;"></div>
+								<div class="panel panel-default col-sm-6 col-sm-offset-3">
+									<div class="panel-body">
+										@if(isset($error))
+											<div class="alert alert-danger text-center">
+												<p><strong>Error de inicio de sesión</strong></p>
+												{{ $error }}
+											</div>
+										@endif
+
+										<div class="separator"></div>
+										<form role="form" action="{{ url('login') }}" method="post" name="formLogin">
+											{{ csrf_field() }}
+											<div class="form-group">
+												<input type="text" class="form-control" name="username" id="username" placeholder="Usuario">
+											</div>
+											<div class="form-group">
+												<input type="password" class="form-control" name="password" id="password" autocomplete="off" placeholder="Contraseña">
+											</div>
+											<input type="submit" class="btn btn-primary btn-block" value="Ingresar" />
+										</form>
+									</div>
+								</div>
+								<div class="clearfix"></div>
+							</div>
+						</div>
+					</div>
+				</div>
+
+			</div>
+		</div>
+	</div>
+@stop
+
+@section('js')
+	<script src="{{ asset('public/js/login.js') }}"></script>
+@stop
